d2254cfda859fe172a7e4e4d3805525e4edf0fbf
Michael Irby <michael.irby@dfinsolutions.com>
2023-10-16 16:06:29 -0400

    Further updates

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,18 @@
 import { FlatList } from 'react-native';
+import ExpenseItem from './ExpenseItem';
 
-const ExpensesList = () => {
-  return <FlatList />;
+const renderExpenseItem = (itemData) => {
+  return <ExpenseItem {...itemData.item} />;
+};
+
+const ExpensesList = ({ expenses }) => {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 };
 
 export default ExpensesList;
